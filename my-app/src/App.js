23d3aeb8441a4c829f432bfbb29ef57f2def50dd
Jason Kundi <jasonkundi1996@gmail.com>
2023-01-11 16:36:20 +0000

    started header page

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="Header"> 
+      <img src="https://images.unsplash.com/photo-1467196271131-42c8c8499d72?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2090&q=80" alt="header" className="Header-img"></img>
+      <h2 className="Heading"> Online Market With The Biggest Product Choice </h2>
+      <p className="Sub-Heading"> In our Online store the biggest choice of products on the planet. Free delivery and a lot of benefits for you yet.</p>
+      <button href='- /* create tag later */ -' className="Discount"> Get Discount for $99 </button>
+       </div>
+    </div>
+  );
+}
+
+export default App;
