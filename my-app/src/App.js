6b8610627011d3ec0d896328dc3a8b89371dc721
Jason Kundi <jasonkundi1996@gmail.com>
2023-01-11 17:35:14 +0000

    header top section positioning complete

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="header-container">
-      <section className="header-left"> 
-      <h2 className="heading"> Online Market With The Biggest Product Choice </h2>
-      <p className="sub-heading"> In our Online store the biggest choice of products on the planet. Free delivery and a lot of benefits for you yet.</p>
-      <button href='- /* create tag later */ -' className="discount"> Get Discount for $99 </button>
+      <section className="header-left">
+        <h2 className="heading">
+          {" "}
+          Online Market With The Biggest Product Choice
+          <p className="sub-heading">
+            {" "}
+            In our Online store the biggest choice of products on the planet.
+            Free delivery and a lot of benefits for you yet.
+          </p>
+        </h2>
+        <button href="- /* create tag later */ -" className="discount">
+          {" "}
+          Get Discount for $99{" "}
+        </button>
       </section>
       <section className="header-right">
-      <img src="https://images.unsplash.com/photo-1467196271131-42c8c8499d72?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2090&q=80" alt="header" className="header-img"></img>
+        <img
+          src="https://images.unsplash.com/photo-1467196271131-42c8c8499d72?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2090&q=80"
+          alt="header"
+          className="header-img"
+        ></img>
       </section>
     </div>
   );
