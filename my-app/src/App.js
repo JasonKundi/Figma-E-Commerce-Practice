d7a989df6f30355448b2dfbd5498aaf0be588c74
Jason Kundi <jasonkundi1996@gmail.com>
2023-01-11 16:58:13 +0000

    header page styling

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <div className="Header"> 
-      <img src="https://images.unsplash.com/photo-1467196271131-42c8c8499d72?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2090&q=80" alt="header" className="Header-img"></img>
-      <h2 className="Heading"> Online Market With The Biggest Product Choice </h2>
-      <p className="Sub-Heading"> In our Online store the biggest choice of products on the planet. Free delivery and a lot of benefits for you yet.</p>
-      <button href='- /* create tag later */ -' className="Discount"> Get Discount for $99 </button>
-       </div>
+    <div className="header-container">
+      <section className="header-left"> 
+      <h2 className="heading"> Online Market With The Biggest Product Choice </h2>
+      <p className="sub-heading"> In our Online store the biggest choice of products on the planet. Free delivery and a lot of benefits for you yet.</p>
+      <button href='- /* create tag later */ -' className="discount"> Get Discount for $99 </button>
+      </section>
+      <section className="header-right">
+      <img src="https://images.unsplash.com/photo-1467196271131-42c8c8499d72?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2090&q=80" alt="header" className="header-img"></img>
+      </section>
     </div>
   );
 }
